86ac78d88cd8cb5fdc30b1cc299af5e0bae4281a
Brian Wilson <bwilson3751@outlook.com>
2017-08-02 23:47:57 -0400

    Fixed indentation

--- install.sh
+++ install.sh
@@ -4,52 +4,52 @@ WORKING_DIRECTORY="/tmp"
 SYS_DEPENDENCIES=('git')
 
 check_if_already_installed() {
-	if [ -d $HOME/.vim ]; then
-		echo "A vim configuration is already in place!"
-		while true; do
-			read -r -p "Would you like to back it up? [y/N]" yn
-			case "$yn" in
-				[Yy] ) backup_previous_config
-					break
+    if [ -d $HOME/.vim ]; then
+        echo "A vim configuration is already in place!"
+        while true; do
+            read -r -p "Would you like to back it up? [y/N]" yn
+            case "$yn" in
+                [Yy] ) backup_previous_config
+                    break
                     ;;
 
-				[Nn] )	break
-					;;
+                [Nn] )	break
+                    ;;
 
-				* ) echo "Please answer yes or no"
-					;;
-			esac
-		done
-	fi
+                * ) echo "Please answer yes or no"
+                    ;;
+            esac
+        done
+    fi
 }
 
 check_dependencies() {
-	echo "Checking dependencies..."
-	for dep in "${SYS_DEPENDENCIES[@]}"; do
-		which "$dep" > /dev/null 2>&1
-		if [ $? -eq 1 ]; then
-			echo "[Error] Broken dependency: $dep"
-			exit 1
-		fi
-	done
+    echo "Checking dependencies..."
+    for dep in "${SYS_DEPENDENCIES[@]}"; do
+        which "$dep" > /dev/null 2>&1
+        if [ $? -eq 1 ]; then
+            echo "[Error] Broken dependency: $dep"
+            exit 1
+        fi
+    done
 }
 
 backup_previous_config() {
-	echo "Backing up previous configuration..."
-	mv $HOME/.vim $HOME/.vim.b-$(date +'%Y%d%m')
+    echo "Backing up previous configuration..."
+    mv $HOME/.vim $HOME/.vim.b-$(date +'%Y%d%m')
 }
 
 install_configuration() {
     echo "Grabbing remote configuration..."
 
-	# Remove conflicting setups if they exist
-	if [ -d $HOME/.vim ]; then
-		rm -rf $HOME/.vim
-		unlink $HOME/.vimrc
-	fi
+    # Remove conflicting setups if they exist
+    if [ -d $HOME/.vim ]; then
+        rm -rf $HOME/.vim
+        unlink $HOME/.vimrc
+    fi
 
-	git clone https://github.com/DrSpeedy/vimrc.git $HOME/.vim
-	ln -s $HOME/.vim/vimrc $HOME/.vimrc
+    git clone https://github.com/DrSpeedy/vimrc.git $HOME/.vim
+    ln -s $HOME/.vim/vimrc $HOME/.vimrc
 }
 
 install_vim_plugins() {
